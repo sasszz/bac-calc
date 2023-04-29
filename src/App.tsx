bd0693f1c6b81b02e2e2b60da6ebe7b282b65dae
sasszz <luchevreuil@gmail.com>
2023-04-28 17:24:20 -0700

    Deploy and Warning

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,15 @@ import Home2 from "./components/Home2";
 
 function App() {
   return (
-    <div className="App container mx-auto">
+    <div className="App container mx-auto h-screen">
       <p className="mt-10 block uppercase tracking-wide text-gray-700 text-xs font-bold mb-5">
         BAC Calculator
       </p>
       <Home2 />
+      <p className="bg-purple-300 p-5 block uppercase tracking-wide text-gray-700 text-xs font-bold fixed bottom-0 left-0 w-screen h-12]">
+        Please note this is for edutainment purposes only and does not guarantee
+        accuracy. Do not drink and drive!
+      </p>
     </div>
   );
 }
